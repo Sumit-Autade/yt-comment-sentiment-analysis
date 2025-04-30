663685164da220b0cabbaacc7a7de2e006e13a0f
Sumit-Autade <sumitautade100@gmail.com>
2025-04-30 14:09:47 +0530

    initial commit with updated cookicutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a chorme plugin to detect the youtube's commenet sentiments',
+    author='sumit autade',
+    license='',
+)
